aba434e0b06fdd7314e6350346de4aa241e19c86
atharelhaidary <atharrelhaidary@gmail.com>
2026-05-23 00:42:23 +0300

    Add vercel.json configuration2

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,33 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
   /* config options here */
+  transpilePackages: ['antd', '@ant-design/icons'],
+  experimental: {
+    // optimizeCss: true,
+    optimizePackageImports: ['antd']
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
+    ],
+    // إضافة هذه الإعدادات لمنع الكوكيز
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    minimumCacheTTL: 60,
+    disableStaticImages: false,
+  }, 
+  compress: true,
+  compiler: {
+    // removeConsole: process.env.NODE_ENV === 'production',
+    reactRemoveProperties: process.env.NODE_ENV === 'production',
+  },
+  
 };
 
 export default nextConfig;
